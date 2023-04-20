546fe481cb5105acf8f1eb87f9d5884eec7226ec
Lukasz Winklarz <lukasz.winklarz@gmail.com>
2023-04-21 00:01:15 +0200

    Linux ARM64 support

--- build/Settings.cs
+++ build/Settings.cs
@@ -44,6 +44,7 @@ namespace Build
             "min.win-x86",
             "min.win-x64",
             "linux-x64",
+            "linux-arm64",
             "osx-x64",
             "osx-arm64",
             "win-x86",
@@ -56,6 +57,7 @@ namespace Build
             { "win-x64", "WINDOWS" },
             { "win-arm64", "WINDOWS"},
             { "linux-x64", "LINUX" },
+            { "linux-arm64", "LINUX" },
             { "osx-x64", "OSX" },
             { "osx-arm64", "OSX" },
             { "min.win-x86", "WINDOWS" },
@@ -87,6 +89,13 @@ namespace Build
             "linux-musl-x64"
         };
 
+        private static readonly string[] _linARMPowershellRuntimes = new[]
+        {
+            "unix",
+            "linux-arm64"
+        };
+
+
         private static readonly string[] _osxPowershellRuntimes = new[]
         {
             "osx",
@@ -109,6 +118,7 @@ namespace Build
                 { "win-x64", _winPowershellRuntimes },
                 { "win-arm64", _winPowershellRuntimes },
                 { "linux-x64", _linPowershellRuntimes },
+                { "linux-arm64", _linARMPowershellRuntimes },
                 { "osx-x64", _osxPowershellRuntimes },
                 { "osx-arm64", _osxARMPowershellRuntimes }
             };
@@ -131,6 +141,7 @@ namespace Build
                     // https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-macos?view=powershell-7.2#supported-versions
                     // That being said, we might as well include "osx" and "unix" since it'll hardly affect package size and should lead to more accurate error messages
                     { "osx-arm64", new [] { "osx", "unix" } }
+                    { "linux-arm64", new [] { "linux", "unix" } }
                 }
             },
             {
